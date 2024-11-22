31d4112e7e02d5286170a5379205a0ddbec47651
RomanPikhotskiy <romapanda5@mail.ru>
2024-11-22 15:09:17 +0300

    asd

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,32 +1,55 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
 
 template<typename T>
-class ArrayHandler {
+class ArrayHandler
+{
 private:
-    size_t _size;
     T* _array;
-public:
-    ArrayHandler(size_t size = 10) {
+    size_t _size;
+    size_t _count;
 
+    void Resize(size_t new_size) {
+        T* new_array = new T[new_size];
+        std::copy(_array, _array + _count, new_array);
+        delete[] _array;
+        _array = new_array;
+        _size = new_size;
     }
+
+public:
+    ArrayHandler() : _size(5), _count(0), _array(new T[_size]) {};
+    
+    ArrayHandler(size_t size) : _size(size), _count(0), _array(new T[_size]) {};
 
     void AppendElem(T elem) {
+        if (_count == _size) {
+            Resize(_size * 2);
+        }
+        _array[_count++] = elem;
+    };
 
-    }
-
-    bool IsContains(T elem) {
-        return false;
-    }
+    bool IsContains(T elem){
+        return std::find(_array, _array + _count, elem) != (_array + _count);
+    };
 
     T GetMax() {
-        return 0;
-    }
+        if (_count == 0) throw std::out_of_range("Array is empty");
+        return *std::max_element(_array, _array + _count);
+    };
 
     T GetMin() {
-        return 0;
+        if (_count == 0) throw std::out_of_range("Array is empty");
+        return *std::min_element(_array, _array + _count);
+    };
+
+    size_t Count() const {
+        return _count;
     }
 
     ~ArrayHandler() {
-
-    }
-
+        delete[] _array;
+    };
 };
